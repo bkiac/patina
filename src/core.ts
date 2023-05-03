37727a02071b414b2e2a55a15f3b0bc0ea888258
bkiac <bkiac@pm.me>
2023-05-03 22:39:48 +0200

    Add class implementation

--- src/core.ts
+++ src/core.ts
@@ -1,91 +1,102 @@
-import {CaughtNonErrorPanic, Panic, PropagationPanic} from "./panic";
+import {CaughtNonErrorPanic, Panic, PropagationPanic} from "./panic"
 
 type Methods<TValue> = {
 	/**
-	 * Unwraps value or throws a special {@link PropagationPanic} that's caught by {@link capture} or {@link captureAsync}.
+	 * Unwraps value or throws a special {@link PropagationPanic} that's caught by {@link capture}.
 	 * Use this method to unwrap the value and propagate potential errors up the call stack.
 	 */
-	propagate: () => TValue;
+	propagate: () => TValue
 	/** Unwraps value, if result is an {@link Err} throw `panic`.  */
-	expect: (panic: Panic | string) => TValue;
+	expect: (panicOrMessage: Panic | string) => TValue
 	/** Unwraps the value, and throw if the result is an {@link Err}. */
-	unwrap: () => TValue;
+	unwrap: () => TValue
 	/** Unwraps the error, and throw if the result is an {@link Ok}. */
-	unwrapErr: () => Error;
+	unwrapErr: () => Error
 	/** Unwraps with a default value provided. */
-	unwrapOr: <T>(defaultValue: T) => T | TValue;
+	unwrapOr: <T>(defaultValue: T) => T | TValue
 	/** Unwraps with a default value provided by a function. */
-	unwrapOrElse: <T>(defaultValue: (error: Error) => T) => T | TValue;
+	unwrapOrElse: <T>(defaultValue: (error: Error) => T) => T | TValue
 	/** Takes an object with two functions `ok` and `err` and executes the corresponding one based on the result type. */
-	match: <T>({ok, err}: {ok: (value: TValue) => T; err: (error: Error) => T}) => T;
-	/** Converts the result into a tuple: `[undefined, TValue]` for {@link Ok} and `[Error, undefined]` for {@link Err}. */
-	toTuple: () => readonly [undefined, TValue] | readonly [Error, undefined];
-};
-
-export interface Ok<T> extends Methods<T> {
-	readonly ok: true;
-	readonly value: T;
-	readonly error?: never;
+	match: <O, E>({ok, err}: {ok: (value: TValue) => O; err: (error: Error) => E}) => O | E
 }
 
-export interface Err extends Methods<never> {
-	readonly ok: false;
-	readonly value?: never;
-	readonly error: Error;
+export class Ok<T> implements Methods<T> {
+	public readonly ok = true
+	public readonly value: T
+	public readonly error?: never
+
+	public constructor(value: T) {
+		this.value = value
+	}
+
+	public propagate = () => this.value
+
+	public expect = () => this.value
+
+	public unwrap = () => this.value
+
+	public unwrapErr = () => {
+		throw new Panic("Cannot unwrap error from Ok result")
+	}
+
+	public unwrapOr = () => this.value
+
+	public unwrapOrElse = () => this.value
+
+	public match = <O, E>(m: {ok: (value: T) => O; err: (error: Error) => E}): O | E =>
+		m.ok(this.value)
+}
+
+export class Err implements Methods<never> {
+	public readonly ok = false
+	public readonly value?: never
+	public readonly error: Error
+
+	public constructor(errorOrMessage: Error | string) {
+		this.error = errorOrMessage instanceof Error ? errorOrMessage : new Error(errorOrMessage)
+	}
+
+	public propagate = () => {
+		throw new PropagationPanic(this.error)
+	}
+
+	public expect = (panicOrMessage: Panic | string) => {
+		if (panicOrMessage instanceof Panic) {
+			throw panicOrMessage
+		}
+		throw new Panic(panicOrMessage)
+	}
+
+	public unwrap = () => {
+		throw new Panic(this.error)
+	}
+
+	public unwrapErr = () => this.error
+
+	public unwrapOr = <T>(defaultValue: T) => defaultValue
+
+	public unwrapOrElse = <T>(defaultValue: (error: Error) => T) => defaultValue(this.error)
+
+	public match = <O, E>(m: {ok: (value: never) => O; err: (error: Error) => E}) => m.err(this.error)
 }
 
 /** Represents the result of an operation that can either succeed with a value or fail */
-export type Result<T> = Ok<T> | Err;
+export type Result<T> = Ok<T> | Err
 
 export function ok<T>(value: T): Ok<T> {
-	const getValue = () => value;
-	return {
-		ok: true,
-		value,
-		propagate: getValue,
-		expect: getValue,
-		unwrap: getValue,
-		unwrapErr: () => {
-			throw new Panic("Cannot unwrap error from Ok result");
-		},
-		unwrapOr: getValue,
-		unwrapOrElse: getValue,
-		match: (m) => m.ok(value),
-		toTuple: () => [undefined, value],
-	};
+	return new Ok(value)
 }
 
 export function err(error: Error | string): Err {
-	const errorInstance = error instanceof Error ? error : new Error(error);
-	return {
-		ok: false,
-		error: errorInstance,
-		propagate: () => {
-			throw new PropagationPanic(errorInstance);
-		},
-		expect: (panic) => {
-			if (panic instanceof Panic) {
-				throw panic;
-			}
-			throw new Panic(panic);
-		},
-		unwrap: () => {
-			throw new Panic(errorInstance);
-		},
-		unwrapErr: () => errorInstance,
-		unwrapOr: (defaultValue) => defaultValue,
-		unwrapOrElse: (defaultValue) => defaultValue(errorInstance),
-		match: (m) => m.err(errorInstance),
-		toTuple: () => [errorInstance, undefined],
-	};
+	return new Err(error)
 }
 
 export function handleError(error: unknown) {
 	if (error instanceof Panic) {
-		throw error;
+		throw error
 	}
 	if (error instanceof Error) {
-		return error;
+		return error
 	}
-	throw new CaughtNonErrorPanic(error);
+	throw new CaughtNonErrorPanic(error)
 }
