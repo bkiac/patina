b60fbc5bee47aa135c39c1189a3db34bd99a44d2
bkiac <bkiac@pm.me>
2023-08-27 20:08:43 +0200

    Remove isPromiseLike helper

--- src/util.ts
+++ src/util.ts
@@ -13,13 +13,3 @@ export type ErrorType<T> = ValueErrorType<T>["error"]
 export type NeverFn = (...args: any[]) => never
 export type Fn = (...args: any[]) => any
 export type AsyncFn = (...args: any[]) => Promise<any>
-
-/**
- * Returns true if the value is `PromiseLike`.
- */
-export function isPromiseLike<T>(v: unknown): v is PromiseLike<T> {
-	return (
-		v instanceof Promise ||
-		(v != null && typeof v === "object" && typeof (v as PromiseLike<T>).then === "function")
-	)
-}
