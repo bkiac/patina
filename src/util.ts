13f6ada004536311e4d01e3a3a0d8639bef38620
bkiac <bkiac@pm.me>
2023-09-24 19:32:44 +0200

    Fix util types

--- src/util.ts
+++ src/util.ts
@@ -1,15 +1,17 @@
+import type {PromiseResult} from "./promise_result"
 import type {Result} from "./result"
 
 export type ValueErrorType<T> = T extends
 	| Result<infer V, infer E>
+	| PromiseResult<infer V, infer E>
 	| Promise<Result<infer V, infer E>>
 	| ((...args: any[]) => Result<infer V, infer E>)
+	| ((...args: any[]) => PromiseResult<infer V, infer E>)
 	| ((...args: any[]) => Promise<Result<infer V, infer E>>)
 	? {value: V; error: E}
 	: never
 export type ValueType<T> = ValueErrorType<T>["value"]
 export type ErrorType<T> = ValueErrorType<T>["error"]
 
-export type NeverFn = (...args: any[]) => never
 export type Fn = (...args: any[]) => any
 export type AsyncFn = (...args: any[]) => Promise<any>
