3f574acbb11da80f6ce60e41982a137b6d24e8e7
bkiac <bkiac@pm.me>
2023-11-02 10:02:39 +0100

    Fix variable shadow

--- src/fn.ts
+++ src/fn.ts
@@ -5,16 +5,14 @@ import {PromiseResult} from "./promise_result"
 
 export type Fn = (...args: any[]) => Result<any, any>
 
-export function fn<T extends Fn>(fn: T) {
-	return function (...args) {
-		return fn(...args)
-	} as T
+export function fn<T extends Fn>(f: T) {
+	return f
 }
 
-export function asyncFn<T extends (...args: any[]) => Promise<Result<any, any>>>(fn: T) {
+export function asyncFn<T extends (...args: any[]) => Promise<Result<any, any>>>(f: T) {
 	return function (
 		...args: Parameters<T>
 	): PromiseResult<ResultValueType<ReturnType<T>>, ResultErrorType<ReturnType<T>>> {
-		return new PromiseResult(fn(...args))
+		return new PromiseResult(f(...args))
 	}
 }
