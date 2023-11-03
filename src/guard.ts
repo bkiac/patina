a6375adc79ff31c41ec149f615aa1bce9282a38d
bkiac <bkiac@pm.me>
2023-11-03 17:30:56 +0100

    Add guardWith

--- src/guard.ts
+++ src/guard.ts
@@ -1,13 +1,32 @@
-import {tryAsyncFn, tryFn} from "./try"
+import {ErrorHandler, ResultError} from "./result_error"
+import {tryAsyncFn, tryAsyncFnWith, tryFn, tryFnWith} from "./try"
 
-export function guard<T extends (...args: any[]) => any>(fn: T) {
+type Fn = (...args: any[]) => any
+type AsyncFn = (...args: any[]) => Promise<any>
+
+export function guard<T extends Fn>(f: T) {
 	return function (...args: Parameters<T>) {
-		return tryFn<ReturnType<T>>(() => fn(...args))
+		return tryFn<ReturnType<T>>(() => f(...args))
 	}
 }
 
-export function guardAsync<T extends (...args: any[]) => Promise<any>>(fn: T) {
+export function guardWith<T extends Fn, E extends ResultError>(f: T, handleError: ErrorHandler<E>) {
+	return function (...args: Parameters<T>) {
+		return tryFnWith<ReturnType<T>, E>(() => f(...args), handleError)
+	}
+}
+
+export function guardAsync<T extends AsyncFn>(fn: T) {
 	return function (...args: Parameters<T>) {
 		return tryAsyncFn<Awaited<ReturnType<T>>>(() => fn(...args))
+	}
+}
+
+export function guardAsyncWith<T extends AsyncFn, E extends ResultError>(
+	fn: T,
+	handleError: ErrorHandler<E>,
+) {
+	return function (...args: Parameters<T>) {
+		return tryAsyncFnWith<Awaited<ReturnType<T>>, E>(() => fn(...args), handleError)
 	}
 }
