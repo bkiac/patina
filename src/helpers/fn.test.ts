4db5f5a6b40740a5038b64c627cb61b97884906c
bkiac <bkiac@pm.me>
2023-11-07 09:50:50 +0100

    Fix fn test

--- src/helpers/fn.test.ts
+++ src/helpers/fn.test.ts
@@ -1,5 +1,15 @@
 import {describe, expect, it, expectTypeOf} from "vitest"
-import {asyncFn, fn, Ok, Err, tryAsyncFn, type PromiseResult, type StdError} from "../internal"
+import {
+	asyncFn,
+	fn,
+	Ok,
+	Err,
+	tryAsyncFn,
+	type PromiseResult,
+	type StdError,
+	type Result,
+	tryFn,
+} from "../internal"
 
 describe.concurrent("fn", () => {
 	it("returns Ok result when provided function does not throw", () => {
@@ -12,6 +22,41 @@ describe.concurrent("fn", () => {
 		const wrappedFn = fn(() => Err("rekt"))
 		const result = wrappedFn()
 		expect(result.unwrapErr()).toEqual("rekt")
+	})
+
+	describe("types", () => {
+		it("returns correct type with function returning Ok | Err", () => {
+			const f = (_arg: number) => {
+				if (Math.random() > 0.5) {
+					return Ok(1)
+				}
+				return Err("error")
+			}
+			const wrapped = fn(f)
+			expectTypeOf(wrapped).parameter(0).toBeNumber()
+			expectTypeOf(wrapped).returns.toEqualTypeOf<Result<number, string>>()
+		})
+
+		it("returns correct type with function returning Ok", () => {
+			const f = (_arg: number) => Ok(1)
+			const wrapped = fn(f)
+			expectTypeOf(wrapped).parameter(0).toBeNumber()
+			expectTypeOf(wrapped).returns.toEqualTypeOf<Result<number, unknown>>()
+		})
+
+		it("returns correct type with function returning Err", () => {
+			const f = (_arg: number) => Err(1)
+			const wrapped = fn(f)
+			expectTypeOf(wrapped).parameter(0).toBeNumber()
+			expectTypeOf(wrapped).returns.toEqualTypeOf<Result<unknown, number>>()
+		})
+
+		it("returns correct type with function returning Result", () => {
+			const f = (_arg: number) => tryFn(() => 1)
+			const wrapped = fn(f)
+			expectTypeOf(wrapped).parameter(0).toBeNumber()
+			expectTypeOf(wrapped).returns.toEqualTypeOf<Result<number, StdError>>()
+		})
 	})
 })
 
@@ -42,18 +87,14 @@ describe.concurrent("asyncFn", () => {
 		})
 
 		it("returns correct type with function returning Promise<Ok>", () => {
-			const f = async (_arg: number) => {
-				return Ok(1)
-			}
+			const f = async (_arg: number) => Ok(1)
 			const wrapped = asyncFn(f)
 			expectTypeOf(wrapped).parameter(0).toBeNumber()
 			expectTypeOf(wrapped).returns.toEqualTypeOf<PromiseResult<number, unknown>>()
 		})
 
 		it("returns correct type with function returning Promise<Err>", () => {
-			const f = async (_arg: number) => {
-				return Err(1)
-			}
+			const f = async (_arg: number) => Err(1)
 			const wrapped = asyncFn(f)
 			expectTypeOf(wrapped).parameter(0).toBeNumber()
 			expectTypeOf(wrapped).returns.toEqualTypeOf<PromiseResult<unknown, number>>()
