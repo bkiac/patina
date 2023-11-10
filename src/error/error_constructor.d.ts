7092aab65f24676923e1c960d84ebcfa430494d4
bkiac <bkiac@pm.me>
2023-11-10 12:50:24 +0100

    Fix build config

---
+++ src/error/error_constructor.d.ts
@@ -0,0 +1,6 @@
+// Extends the ErrorConstructor interface to include an optional `captureStackTrace` method,
+// this is needed for generating declaration files for the library without errors,
+// as in some environments this method may not be available.
+interface ErrorConstructor {
+	captureStackTrace?(targetObject: object, constructorOpt?: Function | undefined): void
+}
