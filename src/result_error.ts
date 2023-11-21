8a2e2bb7a4fba4a91835bcce1805dd9cd123315e
bkiac <bkiac@pm.me>
2023-11-21 08:11:41 +0100

    Update ResultError generic and constructor type

--- src/result_error.ts
+++ src/result_error.ts
@@ -1,16 +1,16 @@
 import {inspectSymbol} from "./util"
 import {formatErrorString} from "./util"
 
-export abstract class ResultError<T extends Error | null = null> implements Error {
+export abstract class ResultError<T extends Error | null = Error | null> implements Error {
 	abstract readonly tag: string
 
 	readonly message: string
 	readonly stack?: string
 	readonly origin: T | null
 
-	constructor(message?: string, origin?: T) {
-		this.message = message ?? ""
-		this.origin = origin ?? null
+	constructor(args: {message?: string; origin?: T} = {}) {
+		this.message = args.message ?? ""
+		this.origin = args.origin ?? null
 
 		if (Error.captureStackTrace) {
 			Error.captureStackTrace(this, this.constructor)
@@ -51,7 +51,7 @@ export class StdError<T = unknown> extends ResultError<Error> {
 			origin instanceof Error
 				? origin
 				: new TypeError(`Unexpected error type: "${String(origin)}"`)
-		super(message)
+		super({message})
 		this.origin = o
 		this.originRaw = origin
 	}
