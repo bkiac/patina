3a64ed9138d39cfeac83ef11ccd1b143f420d3ac
bkiac <bkiac@pm.me>
2024-08-29 11:04:15 +0200

    Add jsdocs

--- src/try.ts
+++ src/try.ts
@@ -2,10 +2,46 @@ import {AsyncResult} from "./async_result";
 import {Panic} from "./error";
 import {type Result, type Err} from "./result";
 
+/**
+ * Creates a scope where you can use `yield*` and `try()` together to unwrap or propagate errors.
+ *
+ * **Note:** This function is synchronous. If you need to use asynchronous operations, use `tryBlockAsync` instead.
+ *
+ * **Example:**
+ *
+ * ```ts
+ * const block = tryBlock(function* () {
+ * 	const x = yield* Ok(1).try();
+ * 	const y = yield* Ok(1).try();
+ * 	return Ok(x + y);
+ * });
+ *
+ * assert.equal(block.unwrap(), 2);
+ * ```
+ */
 export function tryBlock<T, E>(scope: () => Generator<Err<E, never>, Result<T, E>>): Result<T, E> {
 	return scope().next().value;
 }
 
+/**
+ * Creates an async scope where you can use `yield*` and `try()` together to unwrap or propagate errors.
+ *
+ * Any thrown `Error` will be wrapped in a `Panic`.
+ *
+ * **Example:**
+ *
+ * ```ts
+ * const asyncNumber = new AsyncResult(Promise.resolve(Ok(1)));
+ *
+ * const block = await tryBlockAsync(async function* () {
+ * 	const x = yield* Ok(1).try();
+ * 	const y = yield* asyncNumber.try();
+ * 	return Ok(x + y);
+ * });
+ *
+ * assert.equal(block.unwrap(), 2);
+ * ```
+ */
 export function tryBlockAsync<T, E>(
 	scope: () => AsyncGenerator<Err<E, never>, Result<T, E>>,
 ): AsyncResult<T, E> {
