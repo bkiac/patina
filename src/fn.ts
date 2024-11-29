9bf7410443e3eda2e44ca8df7a7b5273c95800c6
bkiac <bkiac@pm.me>
2024-11-29 17:32:15 +0100

    Add fn docs

--- src/fn.ts
+++ src/fn.ts
@@ -1,3 +1,9 @@
+/**
+ * Functions wrappers that infer the return type of a function as a `Result<T, E>`.
+ *
+ * @module
+ */
+
 import type { Result } from "./result.ts";
 import { AsyncResult } from "./async_result.ts";
 import type { InferErr, InferOk } from "./util.ts";
@@ -5,11 +11,20 @@ import type { InferErr, InferOk } from "./util.ts";
 /**
  * Wraps a function that returns any shape of `Result<any, any>` and infers its return type as `Result<T, E>`.
  *
- * **Examples**
+ * Of course, you can just type your function manually with a `Result<T, E>` type if you prefer.
  *
+ * @param f - The function to wrap.
+ * @returns A function that returns a `Result<T, E>`.
+ *
+ * @example
  * ```
  * // (a: number, b: number) => Err<string> | Ok<number>
- * const divide = (a: number, b: number) => b === 0 ? Err("division by zero") : Ok(a / b)
+ * const divide = (a: number, b: number) => {
+ *   if (b === 0) {
+ *     return Err("division by zero")
+ *   }
+ *   return Ok(a / b)
+ * }
  *
  * // (a: number, b: number) => Result<number, string>
  * const wrapped = fn(divide)
@@ -22,32 +37,34 @@ export function fn<A extends any[], R extends Result<any, any>>(
 }
 
 /**
- * Wraps a function that returns any shape of `Promise<Result<any, any>>` and wraps the return value in an `AsyncResult`.
+ * Wraps a function that returns any shape of `Promise<Result<any, any>>` or `AsyncResult<any, any>`
+ * and wraps the return value in an `AsyncResult<T, E>`.
  *
- * **Examples**
+ * This is useful because you do not need to manually convert `Ok` and `Err` types to their async versions and you'll also get a nice `AsyncResult` type instead of a union type.
  *
+ * @param f - The function to wrap.
+ * @returns A function that returns an `AsyncResult<T, E>`.
+ *
+ * @example
  * ```
- * // (a: number, b: number) => Promise<Err<string> | Ok<number>>
- * const divide = async (a: number, b: number) => b === 0 ? Err("division by zero") : Ok(a / b)
- *
- * // (a: number, b: number) => AsyncResult<number, string>
- * const wrapped = asyncFn(divide)
- * // now you can do this:
- * const result = await wrapped(1, 2) // => Result<number, string>
+ * // instead of `Promise<Err<string> | Ok<User>>` return type,
+ * // you get `AsyncResult<User, string>`
+ * const getUser = asyncFn(async (userId: string) => {
+ * 	const user = await findUserById(userId)
+ * 	if (!user) {
+ * 		return Err("user not found")
+ * 	}
+ * 	return Ok(user)
+ * })
  * ```
  */
-export function asyncFn<A extends any[], R extends AsyncResult<any, any>>(
+export function asyncFn<
+	A extends any[],
+	R extends AsyncResult<any, any> | Promise<Result<any, any>>,
+>(
 	f: (...args: A) => R,
-): (...args: A) => AsyncResult<InferOk<Awaited<R>>, InferErr<Awaited<R>>>;
-
-export function asyncFn<A extends any[], R extends Promise<Result<any, any>>>(
-	f: (...args: A) => R,
-): (...args: A) => AsyncResult<InferOk<Awaited<R>>, InferErr<Awaited<R>>>;
-
-// deno-lint-ignore explicit-module-boundary-types
-export function asyncFn(f: any): any {
-	// deno-lint-ignore explicit-function-return-type
-	return function (...args: any[]) {
+): (...args: A) => AsyncResult<InferOk<Awaited<R>>, InferErr<Awaited<R>>> {
+	return function (...args: any[]): AsyncResult<InferOk<Awaited<R>>, InferErr<Awaited<R>>> {
 		return new AsyncResult(f(...args));
 	};
 }
